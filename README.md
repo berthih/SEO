457e74249bd0013d9593cd7c13a2c5c2b8120ece
Berthier Hadrien <berthierhadrien@MacBook-Pro-de-Berthier.local>
2018-07-11 14:42:18 +0200

    [MISC] added README and project repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This project was done by Hadrien Berthier and Korim Dadoud, there is no third member.
---
+++ SeoProject/ngrams.py
@@ -0,0 +1,8 @@
+def ngrams(input, n):
+  input = input.split(' ')
+  output = {}
+  for i in range(len(input)-n+1):
+    g = ' '.join(input[i:i+n])
+    output.setdefault(g, 0)
+    output[g] += 1
+  return output
